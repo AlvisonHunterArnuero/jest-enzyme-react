3f66ebb5876b3b6d3ac45cc0b4c0c24787888f99
Alvison Hunter <alvison@gmail.com>
2022-08-23 17:59:54 -0600

    Enhanced 404 error page

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,15 +1,25 @@
 import React from "react";
-
+const errorImage = "https://res.cloudinary.com/alvison-hunter/image/upload/v1661298881/JestEnzymeReact/404Error_auvbu8.png";
 const NotFound = () => {
   return (
     <>
-      <h1 className="text-info display-2">Uh oh! 404</h1>
-      <p className="text-danger">
-        You're far beyond the border, pal! Go back, home!
-      </p>
-      <button type="button" className="btn btn-outline-danger">
-        Reroute
-      </button>
+      <div className="container-fluid">
+        <div className="row justify-content-between align-items-center">
+          <div className="col-12">
+          <img src={errorImage} class="img-fluid" alt="Uh oh! 404" />
+          </div>
+          </div>
+          <div className="row justify-content-center align-items-center">
+          <div className="col-12">
+            <p className="text-info fs-3 my-2">
+              Hey, now! You're far beyond the border, pal! Go back, home!
+            </p>
+            <button type="button" className="btn btn-outline-danger mb-5">
+              Reroute
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
