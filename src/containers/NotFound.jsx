5bdcd7967b8a4471c49b82e972bd753685e73084
Alvison Hunter <alvison@gmail.com>
2022-08-23 11:34:06 -0600

    Added build files enhancements and notfound component details

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
-const NotFound = () => <h1>NotFound</h1>;
+const NotFound = () => {
+  return (
+    <>
+      <h1 className="text-info display-2">Uh oh! 404</h1>
+      <p className="text-danger">
+        You're far beyond the border, pal! Go back, home!
+      </p>
+      <button type="button" className="btn btn-outline-danger">
+        Reroute
+      </button>
+    </>
+  );
+};
 
 export default NotFound;
