7f1245b2e1d2f6b01c973cfb8c4dd4ccee85ae25
Alvison Hunter <alvison@gmail.com>
2022-08-23 18:07:18 -0600

    Updated build files

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-const errorImage = "https://res.cloudinary.com/alvison-hunter/image/upload/v1661298881/JestEnzymeReact/404Error_auvbu8.png";
+const errorImage =
+  "https://res.cloudinary.com/alvison-hunter/image/upload/v1661298881/JestEnzymeReact/404Error_auvbu8.png";
 const NotFound = () => {
   return (
     <>
       <div className="container-fluid">
         <div className="row justify-content-between align-items-center">
           <div className="col-12">
-          <img src={errorImage} class="img-fluid" alt="Uh oh! 404" />
+            <img src={errorImage} class="img-fluid" alt="Uh oh! 404" />
           </div>
-          </div>
-          <div className="row justify-content-center align-items-center">
+        </div>
+        <div className="row justify-content-center align-items-center">
           <div className="col-12">
             <p className="text-info fs-3 my-2">
               Hey, now! You're far beyond the border, pal! Go back, home!
